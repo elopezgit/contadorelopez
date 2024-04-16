274b387f78050e6cc408702c99e1eb0741208174
Esteban Lopez <lopezestebanalejandro@gmail.com>
2024-04-16 20:22:27 -0300

    agregamos el primer props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Info from "./components/Info";
+
 
 function App() {
   return (
     <main className="container my-5">
       <h1>Primer proyecto con react</h1>
-      
+      <Info comision = 'c89i'></Info>
     </main>
   );
 }
---
+++ src/components/Info.jsx
@@ -0,0 +1,17 @@
+
+
+const Info = (props) => {
+    return (
+        <>
+        <h2>Conceptos de react</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eum
+          harum voluptates corporis, ipsa, numquam nulla illum magni praesentium
+          corrupti iste ipsam necessitatibus enim, quis nemo consectetur quas!
+          Voluptatum, eligendi. <br></br> Comision : {props.comision}
+        </p>
+      </>
+    );
+};
+
+export default Info;
