0eb6ba0aeb1555413b7b68b1ed92acce357c5c14
Esteban Lopez <lopezestebanalejandro@gmail.com>
2024-04-17 14:59:12 -0300

    agregamos un funcion mas corta para sumar numero y restar numero

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -8,11 +8,10 @@ const Contador = () => {
     // aqui creo el state
     const [numero,setNumero] = useState(10);
 
-    const sumar = () =>{
-        //numero++
-        setNumero(numero+1)
-        console.log(numero);
-    }
+    // const sumar = () =>{
+    //     //numero++
+    //     setNumero(numero+1)
+    // }
 
     return (
         <section className="text-center">
@@ -20,8 +19,8 @@ const Contador = () => {
             <h2 >Contador</h2>
             
             <p>{numero}</p>
-            <button className="btn btn-primary me-2" onClick={sumar}>+1</button>
-            <button className="btn btn-danger">-1</button>
+            <button className="btn btn-primary me-2" onClick={()=>setNumero(numero+1)}>+1</button>
+            <button className="btn btn-danger" onClick={()=>setNumero(numero-1)}>-1</button>
 
         </section>
     );
